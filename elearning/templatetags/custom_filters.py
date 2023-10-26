dc0fb3cc83769e7e4880b1ff3e83f11a9aeae612
Juan Rocha <juan.rocha@nc3.lu>
2023-10-26 16:31:22 +0200

    [tags]Added split tag

--- elearning/templatetags/custom_filters.py
+++ elearning/templatetags/custom_filters.py
@@ -4,12 +4,17 @@ register = template.Library()
 
 
 @register.filter
-def is_sticker_unlocked(level, sticker):
-    if level.progress == "100%":
+def is_sticker_unlocked(score, sticker):
+    if score.progress == 1:
         if sticker == "1":
             return True
-        elif sticker == "2" and level.score >= 70:
+        elif sticker == "2" and score.score >= 0.70:
             return True
-        elif sticker == "3" and level.score > 80:
+        elif sticker == "3" and score.score > 0.90:
             return True
     return False
+
+
+@register.filter(name="split")
+def split(value, key):
+    return value.split(key)
