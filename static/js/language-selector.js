36bc86b76dfb67c0929874842d2f94d2b69254e7
Juan Rocha <juan.rocha@nc3.lu>
2024-02-05 16:44:09 +0100

    [language-selector]Improved layout

--- static/js/language-selector.js
+++ static/js/language-selector.js
@@ -1,26 +1,5 @@
 $(document).ready(function () {
-    const languageSelector = $('#language_selector');
-
-    languageSelector.focus(() => {
-        languageSelector.find('option').each(function () {
-            $(this).text($(this).data('fullname'));
-        });
-    });
-
-    languageSelector.change(() => {
-        const selectedOption = languageSelector.find('option:selected');
-        selectedOption.text(selectedOption.val());
-        languageSelector.closest('form').submit();
-    });
-
-    languageSelector.blur(() => {
-        languageSelector.find('option').each(function () {
-            $(this).text($(this).val());
-        });
-    });
-
-    languageSelector.focusout(() => {
-        const selectedOption = languageSelector.find('option:selected');
-        selectedOption.text(selectedOption.val());
+    $('#language_selector').change(function () {
+        $(this).closest('form').submit();
     });
 });
