e91bfa838a0fa0f4d4af837ca3e59c66f70fa40f
gregWDumont <gregory.wd@pm.me>
2024-03-18 17:04:41 +0100

    [Design] Rm JS class toggle for Django in HTML

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,23 +1,7 @@
-function toggleSidebar() {
-    $('#sidebar .d-none').toggleClass('d-sm-inline');
-    $('#sidebar .d-inline').toggleClass('d-sm-none');
-    $('#sidebar').addClass('minimized');
-    $('#logo_small, #open_sidebar').toggleClass('d-sm-none');
-    $('#logo_small').removeClass('d-sm-none');
-    $('#logo_large').addClass('d-none');
-    $('#start-button').addClass('d-none');
-
-    console.log('sidebar toggled');
-}
-
 $(document).ready(function () {
     let windowsWidth = $(window).width();
     let sidebarState = JSON.parse(document.getElementById('sidebarState').textContent);
     console.log(sidebarState);
-
-    if (sidebarState === 'minimized') {
-        toggleSidebar();
-    }
 
     function getCookie(name) {
         let cookieValue = null;
@@ -37,9 +21,7 @@ $(document).ready(function () {
     const csrftoken = getCookie('csrftoken');
 
     $('#sidebarToggle').on("click", function () {
-    
-        $('#sidebar .d-none').toggleClass('d-sm-inline');
-        $('#sidebar .d-inline').toggleClass('d-sm-none');
+        console.log('before ajax:', sidebarState);
         if (sidebarState === 'expanded') {
             $.ajax({
                 type: "POST",
@@ -48,10 +30,6 @@ $(document).ready(function () {
                     'X-CSRFToken': csrftoken
                 },
                 success: function (data) {
-                    $('#sidebar').addClass('minimized');
-                    $('#logo_small').removeClass('d-sm-none');
-                    $('#logo_large').addClass('d-none');
-                    $('#start-button').addClass('d-none');
                     sidebarState = 'minimized';
                     console.log('after ajax:', sidebarState);
                 },
@@ -68,10 +46,6 @@ $(document).ready(function () {
                     'X-CSRFToken': csrftoken
                 },
                 success: function (data) {
-                    $('#sidebar').removeClass('minimized');
-                    $('#logo_small').addClass('d-sm-none');
-                    $('#logo_large').removeClass('d-none');
-                    $('#start-button').removeClass('d-none');
                     sidebarState = 'expanded';
                     console.log('after ajax:', sidebarState);                    
                 },
