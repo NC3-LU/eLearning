543780d2a04d7705dfce129c625905c43902c76d
gregWDumont <gregory.wd@pm.me>
2024-03-20 16:09:13 +0100

    [sidebar] Add Js toggle function

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,9 +1,25 @@
+function toggleSidebar() {
+    $('#sidebar .d-none').toggleClass('d-sm-inline');
+    $('#sidebar .d-inline').toggleClass('d-sm-none');
+    $('#sidebar').toggleClass('minimized');
+    $('#open_sidebar').toggleClass('d-sm-none');
+    $('#logo_small').toggleClass('d-sm-none');
+    $('#logo_large').toggleClass('d-none');
+    $('#start-button').toggleClass('d-none');
+
+    console.log('sidebar toggled');
+}
+
 $(document).ready(function () {
     let windowsWidth = $(window).width();
     let sidebarState = JSON.parse(document.getElementById('sidebarState').textContent);
     const tooltips =  $('[data-toggle="tooltip"]')
     tooltips.tooltip({placement: 'auto'})
     console.log(sidebarState);
+
+    if (sidebarState === 'minimized') {
+        toggleSidebar();
+    }
 
     function getCookie(name) {
         let cookieValue = null;
@@ -24,6 +40,8 @@ $(document).ready(function () {
 
     $('#sidebarToggle').on("click", function () {
         console.log('before ajax:', sidebarState);
+        toggleSidebar();
+
         if (sidebarState === 'expanded') {
             $.ajax({
                 type: "POST",
