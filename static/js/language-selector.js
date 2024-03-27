0f248f4d7c456f17201b032a91fac9431bda7fae
Juan Rocha <juan.rocha@nc3.lu>
2024-03-27 16:12:19 +0100

    [language_selector]Renaed ids of selectors

--- static/js/language-selector.js
+++ static/js/language-selector.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('#language_selector').change(function () {
+    $('#language_selector_expanded, #language_selector_minimized').change(function () {
         $(this).closest('form').submit();
     });
 });
