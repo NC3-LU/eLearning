d3baeca88a59e40068629a05db577fd99345ab48
Juan Rocha <juan.rocha@nc3.lu>
2024-04-15 16:13:33 +0200

    [sidebar]Fixed keep sidebar status

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,13 +1,8 @@
 function toggleSidebar() {
-    $('#sidebar .d-none').toggleClass('d-sm-inline');
-    $('#sidebar .d-inline').toggleClass('d-sm-none');
+    $('#sidebar .switching-item').toggleClass('d-sm-inline');
+    $('#sidebar .switching-item').toggleClass('d-sm-none');
     $('#sidebar').toggleClass('minimized');
-    $('#open_sidebar').toggleClass('d-sm-none');
-    $('#logo_small').toggleClass('d-sm-none');
-    $('#logo_large').toggleClass('d-none');
-    $('#start-button').toggleClass('d-none');
 
-    console.log('sidebar toggled');
 }
 
 $(document).ready(function () {
@@ -15,11 +10,6 @@ $(document).ready(function () {
     let sidebarState = JSON.parse(document.getElementById('sidebarState').textContent);
     const tooltips =  $('[data-toggle="tooltip"]')
     tooltips.tooltip({placement: 'auto'})
-    console.log(sidebarState);
-
-    if (sidebarState === 'minimized') {
-        toggleSidebar();
-    }
 
     function getCookie(name) {
         let cookieValue = null;
@@ -39,53 +29,33 @@ $(document).ready(function () {
     const csrftoken = getCookie('csrftoken');
 
     $('#sidebarToggle').on("click", function () {
-        console.log('before ajax:', sidebarState);
+        let newSidebarState = (sidebarState === 'expanded') ? 'minimized' : 'expanded';
         toggleSidebar();
-
-        if (sidebarState === 'expanded') {
-            $.ajax({
-                type: "POST",
-                url: 'sidebar_state/?state=minimized',
-                headers: {
-                    'X-CSRFToken': csrftoken
-                },
-                success: function (data) {
-                    sidebarState = 'minimized';
-                    console.log('after ajax:', sidebarState);
-                },
-                error: function (data) {
-                    console.log(data);
-                }
-            });
-
-        } else {
-            $.ajax({
-                type: "POST",
-                url: 'sidebar_state/?state=expanded',
-                headers: {
-                    'X-CSRFToken': csrftoken
-                },
-                success: function (data) {
-                    sidebarState = 'expanded';
-                    console.log('after ajax:', sidebarState);
-                },
-                error: function (data) {
-                    console.log(data);
-                }
-            });
-        }
+        $.ajax({
+            type: "POST",
+            url: 'sidebar_state/?state=' + newSidebarState,
+            headers: {
+                'X-CSRFToken': csrftoken
+            },
+            success: function (data) {
+                sidebarState = data.state
+            },
+            error: function (data) {
+                console.log(data);
+            }
+        });
     });
 
     $(window).on('resize', function(){
         var win = $(this);
         if (win.width() <= 992 && sidebarState === 'expanded') {
-            $('#sidebarToggle').trigger( "click" );
+            toggleSidebar();
         }else if (win.width() > 992 && sidebarState === 'minimized'){
-            $('#sidebarToggle').trigger( "click" );
+            toggleSidebar();
         }
     });
 
     if (windowsWidth <= 992 && sidebarState === 'expanded'){
-        $('#sidebarToggle').trigger( "click" );
+        toggleSidebar();
     }
 });
